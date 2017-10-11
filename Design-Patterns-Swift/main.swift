49cc14565fa7dcb29d296bb9db81ab6805302aeb
Rodrigo Filomeno <rodrigofilomeno@mac-mini-de-rodrigo.local>
2017-10-11 12:57:20 -0300

    Initial Commit

---
+++ Design-Patterns-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Design-Patterns-Swift
+//
+//  Created by Rodrigo Filomeno on 11/10/17.
+//  Copyright © 2017 Rodrigo Filomeno. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
